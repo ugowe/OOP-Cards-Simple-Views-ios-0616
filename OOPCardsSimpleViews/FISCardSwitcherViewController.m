ba6143699f133070543bb3106e974cbe84f815d3
ugowe <joe@spheares.com>
2016-06-16 16:59:15 -0400

    add code for OOP Cards Simple Views

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,6 +3,9 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
 
 @end
 
@@ -20,6 +23,32 @@
                                                     rank:@"8"];
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
+    
+    
 }
+- (IBAction)clickThreeOfSpades:(id)sender {
+    self.topLabel.text = self.threeOfSpades.cardLabel;
+    self.middleLabel.text = self.threeOfSpades.cardLabel;
+    self.bottomLabel.text = self.threeOfSpades.cardLabel;
+}
+
+- (IBAction)clickFourOfClubs:(id)sender {
+    self.topLabel.text = self.fourOfClubs.cardLabel;
+    self.middleLabel.text = self.fourOfClubs.cardLabel;
+    self.bottomLabel.text = self.fourOfClubs.cardLabel;
+}
+
+- (IBAction)clickEightOfDiamonds:(id)sender {
+    self.topLabel.text = self.eightOfDiamonds.cardLabel;
+    self.middleLabel.text = self.eightOfDiamonds.cardLabel;
+    self.bottomLabel.text = self.eightOfDiamonds.cardLabel;
+}
+
+- (IBAction)clickTenOfHearts:(id)sender {
+    self.topLabel.text = self.tenOfHearts.cardLabel;
+    self.middleLabel.text = self.tenOfHearts.cardLabel;
+    self.bottomLabel.text = self.tenOfHearts.cardLabel;
+}
+
 
 @end
